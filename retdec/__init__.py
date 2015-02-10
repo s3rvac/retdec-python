d25061f96cba4a6d6d24edd0f3a1ee87a22a10bf
Petr Zemek <s3rvac@gmail.com>
2015-02-10 17:48:57 +0100

    Add the main package (retdec) and a package for tests (tests).

---
+++ retdec/__init__.py
@@ -0,0 +1,11 @@
+"""
+    retdec
+    ~~~~~~
+
+    The main package of a Python library and tools providing easy access to the
+    `retdec.com <https://retdec.com>`_ decompilation service through their
+    public `REST API <https://retdec.com/api/>`_.
+
+    :copyright: © 2015 by Petr Zemek <s3rvac@gmail.com> and contributors
+    :license: MIT, see the ``LICENSE`` file for more details
+"""
---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+    tests
+    ~~~~~
+
+    Unit tests for the project.
+
+    :copyright: © 2015 by Petr Zemek <s3rvac@gmail.com> and contributors
+    :license: MIT, see the ``LICENSE`` file for more details
+"""
