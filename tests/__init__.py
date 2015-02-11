8b9ef7e731a8801535b9348dc0a6869de6c9ecfc
Petr Zemek <s3rvac@gmail.com>
2015-02-11 17:20:37 +0100

    Make the description of the tests package more precise.

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,7 @@
     tests
     ~~~~~
 
-    Unit tests for the project.
+    Tests for the library and tools.
 
     :copyright: © 2015 by Petr Zemek <s3rvac@gmail.com> and contributors
     :license: MIT, see the ``LICENSE`` file for more details
