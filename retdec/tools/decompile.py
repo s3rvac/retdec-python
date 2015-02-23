b9d7de2392652b7347dab0a2cae5d407119cc91c
Petr Zemek <s3rvac@gmail.com>
2015-02-23 17:14:04 +0100

    Implement a base of a tool for decompiling files.
    It is now able to decompile either binary files or C source codes.

--- retdec/tools/decompile.py
+++ retdec/tools/decompile.py
@@ -8,10 +8,46 @@
     :license: MIT, see the ``LICENSE`` file for more details
 """
 
+import argparse
 import sys
+
+from retdec import DEFAULT_API_URL
+from retdec.decompiler import Decompiler
+
+
+def parse_args(argv):
+    """Parses the given list of arguments."""
+    parser = argparse.ArgumentParser(
+        description=('Decompiles the given file through the retdec.com '
+                     'decompilation service by using their public REST API.')
+    )
+    parser.add_argument(
+        'file',
+        metavar='FILE',
+        help='file to decompile'
+    )
+    parser.add_argument(
+        '-k', '--api-key',
+        dest='api_key',
+        metavar='KEY',
+        help='API key to be used'
+    )
+    parser.add_argument(
+        '-u', '--api-url',
+        dest='api_url',
+        metavar='URL',
+        default=DEFAULT_API_URL,
+        help='URL to the API (default: {})'.format(DEFAULT_API_URL)
+    )
+    return parser.parse_args(argv[1:])
 
 
 def main():
+    args = parse_args(sys.argv)
+    decompiler = Decompiler(api_url=args.api_url, api_key=args.api_key)
+    decompilation = decompiler.run_decompilation(input_file=args.file)
+    decompilation.wait_until_finished()
+    decompilation.save_output_hll()
     return 0
 
 
---
+++ tests/tools/decompile_tests.py
@@ -0,0 +1,51 @@
+"""
+    tests.tools.decompile
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Tests for the :mod:`retdec.tools.decompile` module.
+
+    :copyright: © 2015 by Petr Zemek <s3rvac@gmail.com> and contributors
+    :license: MIT, see the ``LICENSE`` file for more details
+"""
+
+import io
+import unittest
+from unittest import mock
+
+from retdec.tools.decompile import parse_args
+
+
+class ParseArgsTests(unittest.TestCase):
+    """Tests for :func:`retdec.tools.decompile.parse_args()`."""
+
+    def setUp(self):
+        # Patch sys.stderr (argparse prints error messages to it).
+        self.stderr = io.StringIO()
+        patcher = mock.patch('sys.stderr', self.stderr)
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
+    def test_file_is_required(self):
+        with self.assertRaises(SystemExit) as cm:
+            parse_args(['decompile.py'])
+        self.assertNotEqual(cm.exception.code, 0)
+
+    def test_file_is_parsed_correctly(self):
+        args = parse_args(['decompile.py', 'prog.exe'])
+        self.assertEqual(args.file, 'prog.exe')
+
+    def test_api_key_is_parsed_correctly_short_form(self):
+        args = parse_args(['decompile.py', '-k', 'KEY', 'prog.exe'])
+        self.assertEqual(args.api_key, 'KEY')
+
+    def test_api_key_is_parsed_correctly_long_form(self):
+        args = parse_args(['decompile.py', '--api-key', 'KEY', 'prog.exe'])
+        self.assertEqual(args.api_key, 'KEY')
+
+    def test_api_url_is_parsed_correctly_short_form(self):
+        args = parse_args(['decompile.py', '-u', 'URL', 'prog.exe'])
+        self.assertEqual(args.api_url, 'URL')
+
+    def test_api_url_is_parsed_correctly_long_form(self):
+        args = parse_args(['decompile.py', '--api-url', 'URL', 'prog.exe'])
+        self.assertEqual(args.api_url, 'URL')
