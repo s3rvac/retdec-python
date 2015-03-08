b2167a7d15ae3bcd6a8de7079a6f6de0c7f28260
Petr Zemek <s3rvac@gmail.com>
2015-03-08 16:31:10 +0100

    Add displaying of progress in the 'decompiler' tool.

--- retdec/tools/decompiler.py
+++ retdec/tools/decompiler.py
@@ -3,15 +3,106 @@
 # Copyright: (c) 2015 by Petr Zemek <s3rvac@gmail.com> and contributors
 # License:   MIT, see the LICENSE file for more details
 #
+# The progress displayers are based on the following script:
+#
+#     https://retdec.com/api/scripts/decompile.py
+#
+#     Copyright (c) 2015 AVG Technologies.
+#
+#     Distributed under the MIT license:
+#
+#     Permission is hereby granted, free of charge, to any person obtaining a
+#     copy of this software and associated documentation files (the
+#     "Software"), to deal in the Software without restriction, including
+#     without limitation the rights to use, copy, modify, merge, publish,
+#     distribute, sublicense, and/or sell copies of the Software, and to permit
+#     persons to whom the Software is furnished to do so, subject to the
+#     following conditions:
+#
+#     The above copyright notice and this permission notice shall be included
+#     in all copies or substantial portions of the Software.
+#
 
 """A tool for decompiling files. It uses the library."""
 
+import abc
 import argparse
 import os
 import sys
 
 from retdec.decompiler import Decompiler
 from retdec.tools import _add_arguments_shared_by_all_tools
+
+
+class ProgressDisplayer(metaclass=abc.ABCMeta):
+    """Base class of progress displayers."""
+
+    @abc.abstractmethod
+    def display_decompilation_progress(self, d):
+        """Displays or updates progress of the given decompilation."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def display_download_progress(self, file):
+        """Displays progress of downloading the given file."""
+        raise NotImplementedError
+
+    def __repr__(self):
+        return '<{}>'.format(self.__class__.__qualname__)
+
+
+class ProgressBarDisplayer(ProgressDisplayer):
+    """Displays a progress bar during decompilation."""
+
+    #: Length of the progress bar (in characters).
+    BAR_LENGTH = 40
+
+    #: Character to be used as the fill symbol.
+    BAR_FILL_CHAR = '#'
+
+    #: Character to be used as the empty symbol.
+    BAR_EMPTY_CHAR = ' '
+
+    def display_decompilation_progress(self, d):
+        # Example:
+        #
+        #     8DRerEdKop: [##################  ] 85%
+        #
+
+        completion = d.get_completion()
+        fill_length = int(self.BAR_LENGTH * (completion / 100))
+
+        # '\r' causes the current line to be overwritten.
+        sys.stdout.write('\r{}: [{}{}] {}% '.format(
+            d.id,
+            self.BAR_FILL_CHAR * fill_length,
+            self.BAR_EMPTY_CHAR * (self.BAR_LENGTH - fill_length),
+            completion
+        ))
+
+        if completion == 100 and d.has_finished():
+            if d.has_succeeded():
+                sys.stdout.write('OK')
+            else:
+                sys.stdout.write('FAIL')
+            sys.stdout.write('\n')
+
+        # Make the output available as soon as possible.
+        sys.stdout.flush()
+
+    def display_download_progress(self, file):
+        # Do not display anything.
+        pass
+
+
+class NoProgressDisplayer(ProgressDisplayer):
+    """Displays nothing."""
+
+    def display_decompilation_progress(self, d):
+        pass
+
+    def display_download_progress(self, file):
+        pass
 
 
 def parse_args(argv):
@@ -42,6 +133,12 @@ def parse_args(argv):
         help='save the outputs into this directory'
     )
     parser.add_argument(
+        '-q', '--quiet',
+        dest='quiet',
+        action='store_true',
+        help='print only errors, nothing else (not even progress)'
+    )
+    parser.add_argument(
         'file',
         metavar='FILE',
         help='file to decompile'
@@ -66,20 +163,38 @@ def get_output_dir(input_file, output_dir):
     return os.path.abspath(os.path.dirname(input_file))
 
 
+def get_progress_displayer(args):
+    """Returns a proper progress displayer based on the arguments provided by
+    the user.
+    """
+    if args.quiet:
+        return NoProgressDisplayer()
+    return ProgressBarDisplayer()
+
+
 def main():
     args = parse_args(sys.argv)
+
     decompiler = Decompiler(
         api_url=args.api_url,
         api_key=args.api_key
     )
+
     decompilation = decompiler.run_decompilation(
         input_file=args.file,
         mode=args.mode
     )
-    decompilation.wait_until_finished()
+
+    displayer = get_progress_displayer(args)
+    displayer.display_decompilation_progress(decompilation)
+    decompilation.wait_until_finished(
+        callback=displayer.display_decompilation_progress
+    )
+
     output_dir = get_output_dir(args.file, args.output_dir)
     decompilation.save_output_hll(output_dir)
     decompilation.save_output_dsm(output_dir)
+
     return 0
 
 
--- tests/tools/decompiler_tests.py
+++ tests/tools/decompiler_tests.py
@@ -6,12 +6,98 @@
 
 """Tests for the :mod:`retdec.tools.decompiler` module."""
 
+import io
 import os
 import unittest
+from unittest import mock
 
+from retdec.decompiler import Decompilation
+from retdec.tools.decompiler import NoProgressDisplayer
+from retdec.tools.decompiler import ProgressBarDisplayer
 from retdec.tools.decompiler import get_output_dir
+from retdec.tools.decompiler import get_progress_displayer
 from retdec.tools.decompiler import parse_args
 from tests.tools import ParseArgsBaseTests
+
+
+class ProgressDisplayerTestsBase(unittest.TestCase):
+    """Base class of progress displayers."""
+
+    def setUp(self):
+        super().setUp()
+
+        # Patch sys.stdout (the displayers print the progress in it).
+        self.stdout = io.StringIO()
+        patcher = mock.patch('sys.stdout', self.stdout)
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
+
+class ProgressBarDisplayerTests(ProgressDisplayerTestsBase):
+    """Tests for :class:`retdec.tools.decompiler.ProgressBarDisplayer`."""
+
+    def test_display_decompilation_progress_successful_decompilation(self):
+        displayer = ProgressBarDisplayer()
+        decompilation = mock.Mock(spec_set=Decompilation)
+        decompilation.id = '8DRerEdKop'
+        decompilation.get_completion.return_value = 100
+        decompilation.has_finished.return_value = True
+        decompilation.has_succeeded.return_value = True
+
+        displayer.display_decompilation_progress(decompilation)
+
+        self.assertEqual(
+            self.stdout.getvalue(),
+            '\r8DRerEdKop: [########################################] 100% OK\n'
+        )
+
+    def test_display_decompilation_progress_failed_decompilation(self):
+        displayer = ProgressBarDisplayer()
+        decompilation = mock.Mock(spec_set=Decompilation)
+        decompilation.id = '8DRerEdKop'
+        decompilation.get_completion.return_value = 100
+        decompilation.has_finished.return_value = True
+        decompilation.has_succeeded.return_value = False
+
+        displayer.display_decompilation_progress(decompilation)
+
+        self.assertEqual(
+            self.stdout.getvalue(),
+            '\r8DRerEdKop: [########################################] 100% FAIL\n'
+        )
+
+    def test_display_download_progress_does_nothing(self):
+        displayer = ProgressBarDisplayer()
+
+        displayer.display_download_progress('test.out.c')
+
+        self.assertEqual(self.stdout.getvalue(), '')
+
+    def test_repr_returns_correct_value(self):
+        displayer = ProgressBarDisplayer()
+        self.assertEqual(repr(displayer), '<ProgressBarDisplayer>')
+
+
+class NoProgressDisplayerTests(ProgressDisplayerTestsBase):
+    """Tests for :class:`retdec.tools.decompiler.NoProgressDisplayer`."""
+
+    def test_display_decompilation_progress_does_nothing(self):
+        displayer = NoProgressDisplayer()
+
+        displayer.display_decompilation_progress(mock.Mock())
+
+        self.assertEqual(self.stdout.getvalue(), '')
+
+    def test_display_download_progress_does_nothing(self):
+        displayer = NoProgressDisplayer()
+
+        displayer.display_download_progress('test.out.c')
+
+        self.assertEqual(self.stdout.getvalue(), '')
+
+    def test_repr_returns_correct_value(self):
+        displayer = NoProgressDisplayer()
+        self.assertEqual(repr(displayer), '<NoProgressDisplayer>')
 
 
 class ParseArgsTests(ParseArgsBaseTests):
@@ -66,6 +152,18 @@ class ParseArgsTests(ParseArgsBaseTests):
         args = parse_args(['decompiler.py', '--output-dir', 'dir', 'prog.exe'])
         self.assertEqual(args.output_dir, 'dir')
 
+    def test_quiet_is_set_to_false_when_not_given(self):
+        args = parse_args(['decompiler.py', 'prog.exe'])
+        self.assertFalse(args.quiet)
+
+    def test_quiet_is_set_to_true_when_given_in_short_form(self):
+        args = parse_args(['decompiler.py', '-q', 'prog.exe'])
+        self.assertTrue(args.quiet)
+
+    def test_quiet_is_set_to_true_when_given_in_long_form(self):
+        args = parse_args(['decompiler.py', '--quiet', 'prog.exe'])
+        self.assertTrue(args.quiet)
+
 
 class GetOutputDirTests(unittest.TestCase):
     """Tests for :func:`retdec.tools.decompiler.get_output_dir()`."""
@@ -91,3 +189,28 @@ class GetOutputDirTests(unittest.TestCase):
             os.path.join(os.getcwd(), 'other_dir')
         )
         self.assertTrue(os.path.isabs(output_dir))
+
+
+class FakeArguments:
+    """Fake representation of tool arguments."""
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+class GetProgressCallbackTests(unittest.TestCase):
+    """Tests for :func:`retdec.tools.decompiler.get_progress_callback()`."""
+
+    def test_returns_progress_bar_displayer_by_default(self):
+        args = FakeArguments(quiet=False)
+
+        displayer = get_progress_displayer(args)
+
+        self.assertIsInstance(displayer, ProgressBarDisplayer)
+
+    def test_returns_no_progress_displayer_if_quiet_is_set(self):
+        args = FakeArguments(quiet=True)
+
+        displayer = get_progress_displayer(args)
+
+        self.assertIsInstance(displayer, NoProgressDisplayer)
