c4bc2b9666ec2fa9bdbd4219304624f4dcd589a9
Petr Zemek <s3rvac@gmail.com>
2016-03-08 14:55:44 +0100

    Bump copyright year to 2015-2016.

--- retdec/fileinfo.py
+++ retdec/fileinfo.py
@@ -1,6 +1,6 @@
 #
 # Project:   retdec-python
-# Copyright: (c) 2015 by Petr Zemek <s3rvac@gmail.com> and contributors
+# Copyright: (c) 2015-2016 by Petr Zemek <s3rvac@gmail.com> and contributors
 # License:   MIT, see the LICENSE file for more details
 #
 
