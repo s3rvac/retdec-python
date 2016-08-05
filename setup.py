d11e8b8fdfa21c1d335bdd37bc63c0c4b794895f
Petr Zemek <s3rvac@gmail.com>
2016-08-05 09:07:49 +0200

    Change the development status in setup.py to "Beta".
    It is no longer in the Alpha state.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     url='https://github.com/s3rvac/retdec-python',
     license=read_file('LICENSE'),
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
